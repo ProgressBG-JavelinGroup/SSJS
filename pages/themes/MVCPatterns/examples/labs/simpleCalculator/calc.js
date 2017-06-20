e202827791508446d0576fe6d361ef9d5383a081
ProgressBG-JavelinGroup <ProgressBG.WWW.Courses+ssjs@gmail.com>
2017-06-20 16:44:11 +0300

    day11+40min:20.06.2017 TODO: slides up, in code TODOs

--- pages/themes/MVCPatterns/examples/labs/simpleCalculator/calc.js
+++ pages/themes/MVCPatterns/examples/labs/simpleCalculator/calc.js
@@ -3,15 +3,18 @@
 //
 var model = {
 	// should come from user
-	data: {
-		x: 5,
-		y: 10,
+	data: {},
+	setData: function(x,y){
+		this.data.x = x;
+		this.data.y = y;
 	},
 	res: 0,
 	calculate: function(x, y){
 		this.res = x+y;
-		view.showResult(res);
 	},
+	updateView: function(){
+		view.showResult(res);
+	}
 }
 
 
@@ -19,11 +22,16 @@ var model = {
 // what user sees
 //
 var view = {
-	nodes: {
-		output: document.getElementsByClassName("output")[0],
-		input: document.getElementsByClassName("input")[0],
-		calcBtn: this.nodes.input.getElementsByClassName('calcBtn')[0],
-	},
+	nodes: (function(){
+		return{
+			// TODO: CAHCE DOM ????
+			output: document.getElementsByClassName("output")[0],
+			input: document.getElementsByClassName("input")[0],
+			op1:document.querySelector(".operand:nth-of-type(1)"),
+			op2:document.querySelector(".operand:nth-of-type(2)"),
+			calcBtn: document.getElementsByClassName('calcBtn')[0],
+		}
+	})(),
 	showResult: function(n){
 		this.nodes.output.innerHTML = n;
 	}
@@ -38,18 +46,24 @@ var controler = {
 	handlers: {
 		'click': function(){
 			model.calculate(model.data.x, model.data.y);
+			this.getData();
 		}
 	},
 	events:{
 		'click': function(){
 			console.dir(view.nodes.calcBtn);
-			view.nodes.calcBtn.addEventListener('click', this.handlers.click);
+			view.nodes.calcBtn.addEventListener('click', controler.handlers.click.bind(this));
 		}
 	},
 	bindEvents: function(){
 		for(let e in this.events){
 			this.events[e]();
 		}
+	},
+	getData:function(){
+		var x = view.op1.value;
+		var y = view.op1.value;
+		model.setData(x,y);
 	},
 	init:function(){
 
